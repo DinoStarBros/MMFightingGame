797befd719d7b2330f0f1ea96c6b604b28dfb874
DinoStarBros <207887083+DinoStarBros@users.noreply.github.com>
2026-04-24 22:49:30 +0800

    bunch of stuff

--- Characters/JohnFight/states/forward_dash.gd
+++ Characters/JohnFight/states/forward_dash.gd
@@ -9,6 +9,8 @@ func enter() -> void:
 	p.character_anim.forward_dash()
 
 func physics_update(delta: float) -> void:
+	p.velocity.x *= p.f_dash_friction
+	
 	frames += 1
 	if frames >= p.f_dash_total_frames:
 		state_machine.change_state("Idle")
